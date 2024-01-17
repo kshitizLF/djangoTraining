69f96c11e36b895457206220532a232c9edd77e6
Kshitiz Sharma <kshitiz.sharma@lambdafunction.ai>
2024-01-17 13:16:23 +0530

    model relationships

--- project1/home/admin.py
+++ project1/home/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 
 from .models import Member,Question,Choice
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date',"question_text"]
+
 admin.site.register(Member)
-admin.site.register(Question)
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
--- project1/home/models.py
+++ project1/home/models.py
@@ -15,6 +15,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField(default = timezone.now)
 
+    def __str__(self):
+        return f"{self.question_text} published on {self.pub_date.date()}"
+    
     def was_published_recently(self):
         return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=3)
 
@@ -24,3 +27,71 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return f"Option {self.choice_text} has {self.votes} votes"
+
+class Owner(models.Model):
+    name = models.CharField(verbose_name="Full Name", max_length=50)
+    age = models.SmallIntegerField()
+    choices = [
+        ("M","Male"),
+        ("F","Female"),
+        ("O","Others")
+    ]
+    sex = models.CharField(verbose_name="Gender",max_length=10,choices=choices)
+    def __str__(self):
+        return f"{self.name}"
+    
+class Car(models.Model):
+    company = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    owner = models.ForeignKey(Owner,on_delete = models.CASCADE)
+
+'''
+    in case of One-to-Many relationship:
+        eg above : one owner can be associated with many cars
+
+        model.object.__dict__ will return a dictionary with all the field of the row fields
+
+        o1.car_set will return objects associated to o1 owner in form of QuerySet
+        
+        o1.car_set.values()[0].get("model")
+
+        o1.car_set.get(model="i20") will return a Car Object
+
+        o1.car_set.get(model="i20").__dict__ will return a dictionary will object's fields as keys
+        
+        can access field of onwer corressponding to a car by : c1.owner.sex
+'''
+
+class Desk(models.Model):
+    number = models.SmallIntegerField(verbose_name="Desk Number")
+    floor = models.SmallIntegerField(verbose_name="Floor No.")
+
+    class Meta:
+        ordering = ['floor','number']
+    
+    def employeeAssigned(self):
+        try:
+            e1 = self.employee
+            return True
+        except:
+            return False
+    
+    
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    choices = [
+        ("IN","Intern"),
+        ("EP","Employee")
+    ]
+    position = models.CharField(max_length = 20,choices = choices)
+    desk = models.OneToOneField(Desk,on_delete=models.CASCADE)
+
+    '''
+        OneToOne relationship : 
+        Only one entry in Employee for a corresspon
+        Some Basic commands
+            Employee.objects.filter(desk__number__gt = 1)
+            Employee.objects.get(name="Abhi").desk
+            Employee.objects.order_by("-name").values()
+    '''
+
