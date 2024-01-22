5b91354bf3d8ed1170dea3f2a983899b2e92aa74
Kshitiz Sharma <kshitiz.sharma@lambdafunction.ai>
2024-01-22 09:51:51 +0530

    Template,Redirect,Detail,List,Form,Create

---
+++ project2/home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+# import home.views as views
+from home import views
+
+urlpatterns = [
+    path("",views.Home.as_view(),name="home"),
+    path("redirectToHome/<int:id>",views.Home2.as_view(),name="redirect"),
+    path("book/<int:pk>",views.Home3.as_view(),name="detail"),
+    path("books/",views.Home4.as_view(),name="bookList"),
+    path("formBook/",views.Home5.as_view(),name="formBook"),
+    path("formSuccess/",views.Home6.as_view(),name="successForm"),
+    path("createBook/",views.Home7.as_view(),name="createBook")
+]
